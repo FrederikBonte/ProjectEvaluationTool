15a86b46e391eeb7717fae1ca66eef0a395e2c2b
Frederik Bonte <fbonte@rocvantwente.nl>
2022-01-14 10:07:54 +0100

    Added to the todo list.

--- index.php
+++ index.php
@@ -14,13 +14,15 @@ include "templates/header.txt";
 	// (ALL PROJECTS ALWAYS HAVE ONE MAIN GROUP with the same id, to keep things simple for now...)
 	//		List of criteria (empty first).
 	// * Rename/edit an existing project...
+	// * NEVER EDIT A PROJECT THAT was already rewarded!!!
+	//   LEFT JOIN with beoordeling and check that MAX(leerlingnummer) is NULL!
 	// @TODO:
 	// * Manage the list of possible reward methods.
 	// * Manage the list of all possible criteria.
 	// * NEVER REMOVE CRITERIA THAT WERE ALREADY AWARDED TO STUDENTS!!!
-	// * NEVER EDIT A PROJECT THAT was already rewarded!!!
-	//   LEFT JOIN with beoordeling and check that MAX(leerlingnummer) is NULL!
+	//   Check that in-use projects cannot be modified in any way. (Also adding new criteria will skew the original rewarding.)
 	// * Create a new editable copy of a project for new classes.
+	//   Original project then always(?) becomes inactive.
 	// * Handle template projects (cannot be used to reward points, only as copy source.)
 	// * Manage the list of teachers.
 	// * Manage the list of classes.
@@ -30,6 +32,11 @@ include "templates/header.txt";
 	// * Choose a project to reward for a student.
 	// * Generate a project evaluation screen for the teacher.
 	// * Process the values entered by the teacher into the final "beoordeling" table.
+	// * Allow subgroups of criteria
+	//    For instance two sub groups, and the main group calculates the average of those.
+	//    Multiple rewards, only the highest is taken into account.
+    //    Implement all the possible group calculations: sum, min, max, average and for now... Other...
+	
 ?>
 </body>
 </html>
