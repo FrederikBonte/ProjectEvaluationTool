6c19f048e0e0ba0dabb1c44355d6efd90cd9fbaa
Frederik Bonte <fbonte@rocvantwente.nl>
2022-01-14 19:51:08 +0100

    Exported the latest version of the database. 0SV1, 0SV2 and 1S have been corrected.

--- export_students.php
+++ export_students.php
@@ -8,11 +8,12 @@ if (array_key_exists("xml", $_REQUEST))
 }
 else
 {
-	header('Content-type: text/plain; charset=utf-8');	
+	header('Content-Type: text/plain; charset=utf-8');	
 }
 if (array_key_exists("klas", $_REQUEST))
 {
 	$klas_id = $_REQUEST["klas"];
+	header('Content-Disposition: inline; filename="students_'.$klas_id.'.'.$mode.'"');
 	if ($mode=="xml")
 	{
 		export_students_xml($klas_id);		
